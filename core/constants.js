20a5866d1ebe7481dd6d09a3aaa8b7168f9fcab1
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-05-27 22:22:50 +0200

    Create constants.js

---
+++ core/constants.js
@@ -0,0 +1,28 @@
+export const CONFIG = {
+  githubUser: 'kevinraphael95',
+  githubRepo: 'notsteam',
+
+  emulatorDataPath:
+    'https://cdn.emulatorjs.org/stable/data/'
+};
+
+export const CONSOLES = [
+  {
+    id: 'nes',
+    name: 'NES',
+    ext: '.nes',
+    core: 'nes',
+    folder: 'nes',
+    icon: '🕹️',
+    color: '#ff5d5d'
+  },
+  {
+    id: 'gba',
+    name: 'Game Boy Advance',
+    ext: '.gba',
+    core: 'gba',
+    folder: 'gba',
+    icon: '🎮',
+    color: '#58a6ff'
+  }
+];
