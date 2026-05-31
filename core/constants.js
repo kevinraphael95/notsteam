901d42eedbe081cce51306978d5b59374b3fd0b3
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-05-31 22:19:36 +0200

    Update constants.js

--- core/constants.js
+++ core/constants.js
@@ -1,9 +1,7 @@
 export const CONFIG = {
   githubUser: 'kevinraphael95',
   githubRepo: 'notsteam',
-
-  emulatorDataPath:
-    'https://cdn.emulatorjs.org/stable/data/'
+  emulatorDataPath: 'https://cdn.emulatorjs.org/stable/data/'
 };
 
 export const CONSOLES = [
@@ -13,16 +11,46 @@ export const CONSOLES = [
     ext: '.nes',
     core: 'nes',
     folder: 'nes',
-    icon: '🕹️',
-    color: '#ff5d5d'
+    thumb: 'assets/consoles/NES.jpg'
+  },
+  {
+    id: 'snes',
+    name: 'Super NES',
+    ext: '.sfc',
+    core: 'snes9x',
+    folder: 'snes',
+    thumb: 'assets/consoles/SNES.jpg'
+  },
+  {
+    id: 'gb',
+    name: 'Game Boy',
+    ext: '.gb',
+    core: 'gambatte',
+    folder: 'gb',
+    thumb: 'assets/consoles/GAMEBOY.jpg'
   },
   {
     id: 'gba',
-    name: 'Game Boy Advance',
+    name: 'Game Boy Adv.',
     ext: '.gba',
     core: 'gba',
     folder: 'gba',
-    icon: '🎮',
-    color: '#58a6ff'
+    thumb: 'assets/consoles/GAMEBOYADV.jpg'
+  },
+  {
+    id: 'n64',
+    name: 'Nintendo 64',
+    ext: '.z64',
+    core: 'n64',
+    folder: 'n64',
+    thumb: 'assets/consoles/NINTENDO64.jpg'
+  },
+  {
+    id: 'ps1',
+    name: 'PlayStation 1',
+    ext: '.bin',
+    core: 'psx',
+    folder: 'ps1',
+    thumb: 'assets/consoles/PLAYSTATION.jpg'
   }
 ];
