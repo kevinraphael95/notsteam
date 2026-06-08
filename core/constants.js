97af196025b83ea820121779332af2512da9ace8
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-06-08 21:46:11 +0200

    Update constants.js

--- core/constants.js
+++ core/constants.js
@@ -8,49 +8,61 @@ export const CONSOLES = [
   {
     id: 'nes',
     name: 'NES',
+    short: 'NES',
     ext: '.nes',
     core: 'nes',
     folder: 'nes',
-    thumb: 'assets/consoles/NES.jpg'
+    icon: '🕹️',
+    color: '#e8192c'
   },
   {
     id: 'snes',
     name: 'Super NES',
+    short: 'SNES',
     ext: '.sfc',
     core: 'snes9x',
     folder: 'snes',
-    thumb: 'assets/consoles/SNES.jpg'
+    icon: '🎮',
+    color: '#7b5ea7'
   },
   {
     id: 'gb',
     name: 'Game Boy',
+    short: 'GB',
     ext: '.gb',
     core: 'gambatte',
     folder: 'gb',
-    thumb: 'assets/consoles/GAMEBOY.jpg'
+    icon: '📺',
+    color: '#8bac0f'
   },
   {
     id: 'gba',
     name: 'Game Boy Adv.',
+    short: 'GBA',
     ext: '.gba',
     core: 'gba',
     folder: 'gba',
-    thumb: 'assets/consoles/GAMEBOYADV.jpg'
+    icon: '🎯',
+    color: '#1a1fd4'
   },
   {
     id: 'n64',
     name: 'Nintendo 64',
+    short: 'N64',
     ext: '.z64',
     core: 'n64',
     folder: 'n64',
-    thumb: 'assets/consoles/NINTENDO64.jpg'
+    icon: '🌀',
+    color: '#009ac7'
   },
   {
     id: 'ps1',
     name: 'PlayStation 1',
+    short: 'PS1',
     ext: '.bin',
     core: 'psx',
     folder: 'ps1',
-    thumb: 'assets/consoles/PLAYSTATION.jpg'
+    icon: '🔵',
+    color: '#003087'
   }
 ];
